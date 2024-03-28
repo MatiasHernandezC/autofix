136d05099372910611d0818fce69f60a15b9f27d
MatiasHernandezC <matias.hernandez.ca@usach.cl>
2024-03-28 02:37:57 -0300

    Primer Commit

---
+++ src/main/java/com/monolitico/autofix/entities/VehiculoEntity.java
@@ -0,0 +1,26 @@
+package com.monolitico.autofix.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "Vehiculos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehiculoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String Patente;
+    private String Marca;
+    private String Modelo;
+    private Date Ano_fabricacion;
+    private String Tipo_motor;
+    private Integer N_asientos;
+}
---
+++ src/main/java/com/monolitico/autofix/repositories/VehiculoRepository.java
@@ -0,0 +1,9 @@
+package com.monolitico.autofix.repositories;
+
+import com.monolitico.autofix.entities.VehiculoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehiculoRepository extends JpaRepository<VehiculoEntity, String> {
+
+
+}
