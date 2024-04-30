5e2204173402b25c94127ba15a37caad5d181f28
MatiasHernandezC <matias.hernandez.ca@usach.cl>
2024-04-30 08:21:37 -0400

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,33 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import CreateStudent from './components/CreateStudent.jsx'
+import StudentList from './components/StudentList.jsx'
+import StudentView from './components/StudentView.jsx'
+import Button from './components/Button.jsx'
+import Example from './components/Example.jsx'
+import CreateStudentRefactor from './components/CreateStudentRefactor.jsx'
+import Form from './components/Form.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header/>            
+      <Routes> 
+        <Route path="/estudiantes" element={<StudentList/>}/>
+        <Route path="/estudiantes/:rutEstudiante" element={<StudentView/>}/>
+        {/* <Route path="/estudiantes/crear" element={<CreateStudent />}/>         */}
+        <Route path="/estudiantes/crear" element={<CreateStudentRefactor />}/>        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/services/gestion.service.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const ESTUDIANTES_API_URL = "http://mingeso2024.brazilsouth.cloudapp.azure.com/api/estudiantes";
+const CARRERAS_API_URL = "http://mingeso2024.brazilsouth.cloudapp.azure.com/api/carreras";
+
+function crearEstudiante(estudiante) {
+  return axios.post(ESTUDIANTES_API_URL, estudiante);  
+}
+
+function getEstudiantes() {
+  return axios.get(ESTUDIANTES_API_URL)
+}
+
+function getEstudiante(rutEstudiante) {  
+  return axios.get(`${ESTUDIANTES_API_URL}/${rutEstudiante}`)
+}
+
+function getCarreras() {
+  return axios.get(CARRERAS_API_URL);
+}
+
+export default {crearEstudiante, getEstudiante, getEstudiantes, getCarreras}
